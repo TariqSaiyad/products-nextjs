a2a43df8a9551daf8370a3876e2bd84188920f4c
Tariq Saiyad <tariqsaiyad98@gmail.com>
2024-08-11 13:30:58 +1200

    remove products

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import Header from "@/components/header";
-import ProductsList from "@/components/product-list";
 
-export const revalidate = 10
-
+export const revalidate = 10;
 
 // This component is rendered on '/'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col px-24 py-8">
       <Header title="Products" />
-      <ProductsList />
+      {/* <ProductsList /> */}
     </main>
   );
 }
