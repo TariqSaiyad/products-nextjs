bebe010f63136b2ee0ac78cc7f6b1cbf3c6e6b2e
Tariq Saiyad <tariqsaiyad98@gmail.com>
2024-08-11 13:18:42 +1200

    fix for product fetch

--- lib/product.ts
+++ lib/product.ts
@@ -1,7 +1,6 @@
 import { Product } from "@/types";
 
 export async function getAllProducts(limit = 30) {
-  console.time("getAllProducts");
   const response = await fetch(
     `https://dummyjson.com/products?limit=${limit}`,
     {
@@ -9,15 +8,16 @@ export async function getAllProducts(limit = 30) {
     }
   );
   const json = await response.json();
-  console.timeEnd("getAllProducts");
   return json.products as Product[];
 }
 
 export async function getProduct(id: string) {
-  console.time(`getProduct: ${id}`);
   const response = await fetch(`https://dummyjson.com/products/${id}`, {
     next: { revalidate: 10 }
   });
-  console.timeEnd(`getProduct: ${id}`);
-  return (await response.json()) as Product;
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+
+  return data as Product;
 }
